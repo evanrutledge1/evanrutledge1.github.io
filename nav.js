d760b081d3000a7f71b38bb4cc0357fa1f596961
evanrutledge1 <168851101+evanrutledge1@users.noreply.github.com>
2024-05-03 11:56:25 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -2,3 +2,7 @@ document.write('<div class="w3-sidebar w3-bar-block w3-card w3-dark-grey" style=
 document.write('<a href="/games/index.html" class="w3-bar-item w3-button w3-black">Games</a>');
 document.write('<a href="http://youtube.com/@sussteve226" class="w3-bar-item w3-button w3-black">Youtube</a>');
 document.write('<a href="http://github.com/evanrutledge226" class="w3-bar-item w3-button w3-black">Github</a></div><div style="margin-left:25%">');
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("w3-dark-grey");
+}
