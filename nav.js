d8d73aae73072b92fd5a4843e11c51fb9bc22a26
evanrutledge1 <168851101+evanrutledge1@users.noreply.github.com>
2024-05-03 12:00:31 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -2,7 +2,13 @@ document.write('<div class="w3-sidebar w3-bar-block w3-card w3-dark-grey" style=
 document.write('<a href="/games/index.html" class="w3-bar-item w3-button w3-black">Games</a>');
 document.write('<a href="http://youtube.com/@sussteve226" class="w3-bar-item w3-button w3-black">Youtube</a>');
 document.write('<a href="http://github.com/evanrutledge226" class="w3-bar-item w3-button w3-black">Github</a></div><div style="margin-left:25%">');
+document.write('<button class="w3-bar-item w3-btn w3-light-grey" onclick="darkMode()">Toggle dark mode</button>');
+document.write('<button class="w3-bar-item w3-btn w3-dark-grey" onclick="lightMode()">Toggle light mode</button>');
 function darkMode() {
   var element = document.body;
   element.classList.toggle("w3-dark-grey");
+}
+function lightMode() {
+  var element = document.body;
+  element.classList.toggle("w3-light-grey");
 }
