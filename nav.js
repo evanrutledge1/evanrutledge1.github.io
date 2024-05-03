07e77b1ce2f3117db924c6447cb1b5a5b24ec43f
evanrutledge1 <168851101+evanrutledge1@users.noreply.github.com>
2024-05-03 11:41:16 -0500

    Create nav.js

---
+++ nav.js
@@ -0,0 +1,4 @@
+document.write('<a href="index.html" class="w3-button w3-black">Home</a>');
+document.write('<a href="/games/index.html" class="w3-button w3-black">Games</a>');
+document.write('<a href="http://youtube.com/@sussteve226" class="w3-button w3-black">Youtube</a>');
+document.write('<a href="http://github.com/evanrutledge226" class="w3-button w3-black">Github</a>');
