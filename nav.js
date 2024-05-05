0ff2edf2c4ffa798950bf0201e9e3bcb4388e332
evanrutledge1 <168851101+evanrutledge1@users.noreply.github.com>
2024-05-05 08:24:25 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,3 +1,4 @@
+document.write('<button class="w3-btn w3-left w3-xlarge w3-light-grey" onclick="w3_open()">&#9776;</button>')
 document.write('<div class="w3-left w3-sidebar w3-bar-block w3-border w3-card w3-dark-grey" id="sidebar" style="width:25%">');
 document.write('<button class="w3-bar-item w3-btn w3-light-grey" onclick="w3_close()">Close &times;</button>');
 document.write('<a href="/index.html" class="w3-bar-item w3-button w3-black">Home</a>');
@@ -5,7 +6,6 @@ document.write('<a href="/games/index.html" class="w3-bar-item w3-button w3-bord
 document.write('<a href="http://youtube.com/@sussteve226" class="w3-bar-item w3-button w3-border w3-black">Youtube</a>');
 document.write('<a href="http://github.com/evanrutledge226" class="w3-bar-item w3-button w3-border w3-black">Github</a>');
 document.write('<button class="w3-bar-item w3-btn w3-border w3-light-grey" onclick="darkMode()">Toggle dark mode</button></div><div style="margin-left:25%"></div>');
-document.write('<button class="w3-btn w3-right w3-xlarge w3-light-grey" onclick="w3_open()">&#9776;</button>')
 function darkMode() {
   var element = document.body;
   element.classList.toggle("w3-dark-grey");
